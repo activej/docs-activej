6f757d5f35789cb6f3ad236963ddde1c1076dd99
Eduard Vasinskyi <evasinskyi@datakernel.io>
2022-06-21 18:48:25 +0300

    Deploy website - based on 803351e5309024cd99dedb5b85a98cd65e102ec9

--- ru/assets/js/ef798800.eb2e4516.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkactivejdocs=self.webpackChunkactivejdocs||[]).push([[2534],{43143:function(e){e.exports=JSON.parse('{"permalink":"/ru/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":7,"previousPage":null,"nextPage":null,"blogDescription":"\u0411\u043b\u043e\u0433","blogTitle":"\u0411\u043b\u043e\u0433"}')}}]);
